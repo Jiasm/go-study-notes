a14417213514604875a7ff4d2b8c5f2ce14a16f9
jiashunming <jiashunming@outlook.com>
2017-03-12 14:45:54 +0800

    update

---
+++ method/struct-method.go
@@ -0,0 +1,27 @@
+package main 
+
+import (
+  "fmt"
+)
+
+type user struct {
+  name string
+  age byte
+}
+
+func (u user) ToString() string {
+  return fmt.Sprintf("%+v", u)
+}
+
+type manager struct {
+  user
+  title string
+}
+
+func main() {
+  var m manager
+  m.name = "Niko"
+  m.age = 29
+
+ println(m.ToString())
+}
