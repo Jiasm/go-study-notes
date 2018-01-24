5bb7626b940550b536872db54e048311af12e54e
jiashunming <jiashunming@outlook.com>
2018-01-24 20:18:10 +0800

    update

---
+++ array/append2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := []int{4: 3}
+	fmt.Printf("%p\n", &s)
+
+	s2 := append(s, 1) // 如果超过原数组上限，则会重新在内存中创建一个数组
+	fmt.Printf("%p\n", &s2)
+
+	fmt.Println(s, s2, cap(s), cap(s2))
+}
